37a3d7523ef7ad160007ae7edfefd8e489e0f375
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2024-09-03 18:53:01 -0700

    Autorun.py

--- Autorun.py
+++ Autorun.py
@@ -1,6 +1,10 @@
 from Autorun.TikTok import tt_functions
 from Autorun.Instagram import inst_functions
+from Autorun.ChatGPT import gpt_functions
 
 # Ejecutar ciclo indefinidamente
-while True:
-   inst_functions.inst_ciclo_bot(70, 60*60)
+#while True:
+#   inst_functions.inst_ciclo_bot(70, 60*60)
+#   tt_functions.tt_ciclo_bot(70, 60*60)
+
+gpt_functions.gpt_ciclo_resp()
