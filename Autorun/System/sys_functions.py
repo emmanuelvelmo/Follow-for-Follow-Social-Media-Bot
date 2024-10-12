67358066fde031a82ebb68b449a1eaa3253aea1e
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2024-10-12 18:54:46 +0000

    sys_functions.py

--- Autorun/System/sys_functions.py
+++ Autorun/System/sys_functions.py
@@ -1,6 +1,13 @@
 import pyautogui
 import time
 
+# Cerrar ventana de resolución
+def sys_cerrar_res():
+    time.sleep(2)
+    pyautogui.moveTo(850, 90)
+    pyautogui.click()
+    time.sleep(1)
+
 # Cerrar ventana
 def sys_cerrar_ventana():
     pyautogui.hotkey('alt', 'f4')
@@ -9,58 +16,19 @@ def sys_cerrar_ventana():
 # Abrir Chromium
 def sys_abrir_chr():
     # Abrir Chromium desde el menú de aplicaciones
-    pyautogui.moveTo(16, 586)
-    pyautogui.click()
-    time.sleep(0.25)
-    pyautogui.moveTo(100, 440)
-    pyautogui.click()
-    time.sleep(0.25)
-    pyautogui.moveTo(300, 440)
-    pyautogui.click()
+    pyautogui.press('win')
+    time.sleep(1)
+    pyautogui.typewrite("chromium")
+    pyautogui.press('enter')
     time.sleep(20)
-    
-    # Pantalla completa
-    pyautogui.press('f11')
-    time.sleep(2)
-
-# Abrir Instagram
-def sys_abrir_inst():
-    pyautogui.moveTo(16, 586)
-    pyautogui.click()
-    time.sleep(0.25)
-    pyautogui.moveTo(100, 440)
-    pyautogui.click()
-    time.sleep(0.25)
-    pyautogui.moveTo(300, 494)
-    pyautogui.click()
-    time.sleep(30)
     pyautogui.press('f11')
     time.sleep(1)
 
-# Abrir Facebook
-def sys_abrir_fb():
-    pyautogui.moveTo(16, 586)
-    pyautogui.click()
-    time.sleep(0.25)
-    pyautogui.moveTo(100, 440)
-    pyautogui.click()
-    time.sleep(0.25)
-    pyautogui.moveTo(300, 470)
-    pyautogui.click()
-    time.sleep(30)
-    pyautogui.press('f11')
+# Abrir Waydroid
+def sys_abrir_wd():
+    # Abrir Chromium desde el menú de aplicaciones
+    pyautogui.press('win')
     time.sleep(1)
-
-# Abrir TikTok
-def sys_abrir_tt():
-    pyautogui.moveTo(16, 586)
-    pyautogui.click()
-    time.sleep(0.25)
-    pyautogui.moveTo(100, 440)
-    pyautogui.click()
-    time.sleep(0.25)
-    pyautogui.moveTo(300, 520)
-    pyautogui.click()
-    time.sleep(50)
-    pyautogui.press('f11')
-    time.sleep(1)
+    pyautogui.typewrite("waydroid")
+    pyautogui.press('enter')
+    time.sleep(60)
